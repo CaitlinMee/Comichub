ab7cf5c12e640ffce8599227e59b3a050fd240cb
CaitlinMee <57130231+CaitlinMee@users.noreply.github.com>
2020-03-09 18:39:04 +1100

    character info

--- src/app/character-info/character-info.component.ts
+++ src/app/character-info/character-info.component.ts
@@ -1,44 +1,9 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Comics, Character } from "../shared/Comics";
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { RestApiService } from '../shared/rest-api.service';
-// import { Location } from '@angular/common';
-
-
-// @Component({
-//   selector: 'character-info',
-//   templateUrl: './character-info.component.html',
-//   styleUrls: ['./character-info.component.css']
-// })
-// export class CharacterInfoComponent implements OnInit {
-//   comic: Comics;
-//   character: Character
-//   display = 'none';
-
-//   constructor(
-//     private route: ActivatedRoute,
-//     public restApi: RestApiService,
-//     private location: Location,
-//     public router: Router
-//   ) { }
-
-//   ngOnInit() {
-//     this.getComic();
-//   }
-
-//   getComic() {
-//     // Get Comic Id and set to array element number
-//     var id = +this.route.snapshot.paramMap.get('id');
-//     id--;
-//     return this.restApi.getComics().subscribe((data: {}) => {
-//       this.comic = data[id];
-//     })
-//   }
-// }
-
-import { Component, OnInit } from '@angular/core';
-import { RestApiService } from '../shared/rest-api.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Comics, Character } from "../shared/Comics";
 import { ActivatedRoute, Router } from '@angular/router';
+import { RestApiService } from '../shared/rest-api.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'character-info',
@@ -46,24 +11,27 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./character-info.component.css']
 })
 export class CharacterInfoComponent implements OnInit {
-
-  Comics: {};
+  comic: Comics;
+ @Input() character: Character;
+  display = 'none';
 
   constructor(
-    public rest:RestApiService,
-    public restApi: RestApiService,
     private route: ActivatedRoute,
-    private router: Router) { }
+    public restApi: RestApiService,
+    private location: Location,
+    public router: Router
+  ) { }
 
-    ngOnInit() {
-      this.loadComics()
-    }
-
-    // Get comics list
-    loadComics() {
-      return this.restApi.getComics().subscribe((data: {}) => {
-        this.Comics = data;
-      })
-    }
+  ngOnInit() {
+    this.getComic();
   }
 
+  getComic() {
+    // Get Comic Id and set to array element number
+    var id = +this.route.snapshot.paramMap.get('id');
+    id--;
+    return this.restApi.getComics().subscribe((data: {}) => {
+      this.comic = data[id];
+    })
+  }
+}
